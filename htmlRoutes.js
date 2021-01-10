553388ee94babcd9b845d3e14eb761ea8d1a441f
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-01-09 18:04:47 -0600

    created the html Routes

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,2 +1,13 @@
 //Depnedencies
 var path = require("path");
+
+module.exports = function(app) {
+    //get requests
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+}
